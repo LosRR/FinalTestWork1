d5d864b31af686d6f87fb3a97d6522a2843b17ad
LosRR <losrr@mail.ru>
2022-12-30 19:23:46 +0300

    код готов

--- code/Program.cs
+++ code/Program.cs
@@ -37,3 +37,38 @@ void PrintArray(string[] array)
 }
 
 int n = ChooseArray("Выберите массив строк для обработки: ");
+string[] array = new string[] { };
+switch (n)
+{
+    case 1:
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case 2:
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case 3:
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{array} - Такой команды нет");
+        break;
+}
+int newArrayLength = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) newArrayLength++;
+}
+string[] newArray = new string[newArrayLength];
+int place = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[place] = array[i];
+        place++;
+    }
+}
+Console.WriteLine("Из выбранного массива сформирован массив по указанным условиям задачи: ");
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
